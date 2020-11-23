ea5befdd6b7c7fc0c775aa16d429f14cfec095dc
nickjlupu <nicklupu7@gmail.com>
2020-11-22 22:04:23 -0600

    Update logicChallenge.js

--- docs/logicChallenge.js
+++ docs/logicChallenge.js
@@ -98,6 +98,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 		}
 		return magnitude * 4;
 	}
+		
+	function popupDate(feature){
+		let display = Date(feature * 1000);
+		console.log(display) 
+		return display
+	} 
 	
 	// Creating a GeoJSON layer with the retrieved data.
 	L.geoJson(data, {
@@ -111,7 +117,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 			// We create a popup for each circleMarker to display the magnitude and
 			// location of the earthquake after the marker has been created and styled.
 			onEachFeature: function(feature, layer) {
-				layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+				layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Date: " + popupDate(feature.properties.time));
 			}
 	}).addTo(earthquakes);
 
